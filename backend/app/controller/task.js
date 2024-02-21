ce2933d7a5ac59a1b09ff69254ae082ee8fc21f7
Вадим <vadimzooru@gmail.com>
2024-02-21 20:04:19 +0800

    added all task handler

--- backend/app/controller/task.js
+++ backend/app/controller/task.js
@@ -1,6 +1,7 @@
 var db = require('../config/db.config');
 var globalFunctions = require('../config/global.function');
 var task = db.task;
+const { Op } = require("sequelize");
 
 exports.findAll = (req,res) =>{
     task.findAll()
@@ -10,4 +11,128 @@ exports.findAll = (req,res) =>{
     .catch(err=>{
         globalFunctions.sendError(res,err);
     })
+}
+
+exports.findById = (req,res) =>{
+    task.findByPk(req.params.id)
+    .then(object=>{
+        globalFunctions.sendResult(res,object);
+    })
+    .catch(err=>{
+        globalFunctions.sendError(res,err);
+    })
+}
+
+exports.create = (req, res) => {
+    task.create({
+        teacher_discipline_id: req.body.teacher_discipline_id,
+        user_id: req.body.user_id,
+        prog_language_id: req.body.prog_language_id,
+        name: req.body.name,
+        description: req.body.description
+        // mark: req.body.mark,
+        // comment: req.body.comment,
+        // file: req.body.file
+    })
+    .then(object => {
+        globalFunctions.sendResult(res, object);
+    })
+    .catch(err => {
+        globalFunctions.sendError(res, err);
+    });
+}
+
+exports.update = (req, res) => {
+    task.update({
+        teacher_discipline_id: req.body.teacher_discipline_id,
+        user_id: req.body.user_id,
+        prog_language_id: req.body.prog_language_id,
+        name: req.body.name,
+        description: req.body.description,
+        mark: req.body.mark,
+        comment: req.body.comment,
+        file: req.body.file
+    }, {
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(object=>{
+        globalFunctions.sendResult(res,object);
+    })
+    .catch(err=>{
+        globalFunctions.sendError(res,err);
+    })
+}
+
+exports.delete = (req, res) => {
+    task.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(object=>{
+        globalFunctions.sendResult(res,"Запись удалена");
+    })
+    .catch(err=>{
+        globalFunctions.sendError(res,err);
+    })
+}
+
+exports.findByProgLanguage = (req, res) => {
+    task.findAll({
+        where: {
+            prog_language_id: req.params.id
+        }
+    })
+    .then(objects => {
+        globalFunctions.sendResult(res, objects);
+    })
+    .catch(err => {
+        globalFunctions.sendError(res, err);
+    });
+}
+
+exports.findByUserId = (req, res) => {
+    task.findAll({
+        where: {
+            user_id: req.params.id
+        }
+    })
+    .then(objects => {
+        globalFunctions.sendResult(res, objects);
+    })
+    .catch(err => {
+        globalFunctions.sendError(res, err);
+    });
+}
+
+exports.findByTeacherDisciplineId = (req, res) => {
+    task.findAll({
+        where: {
+            teacher_discipline_id: req.params.id
+        }
+    })
+    .then(objects => {
+        globalFunctions.sendResult(res, objects);
+    })
+    .catch(err => {
+        globalFunctions.sendError(res, err);
+    });
+}
+
+exports.findByFileUploaded = (req, res) => {
+    task.findAll({
+        where: {
+            file: {
+                [Op.not]: ''
+            }
+        }
+    })
+    .then(objects => {
+        globalFunctions.sendResult(res, objects);
+    })
+    .catch(err => {
+        globalFunctions.sendError(res, err);
+    });
 }
--- backend/app/route/task.js
+++ backend/app/route/task.js
@@ -2,4 +2,12 @@ module.exports = (app) =>{
     const task = require('../controller/task.js');
 
     app.get('/api/listTasks',task.findAll);
+    app.get('/api/task/:id',task.findById);
+    app.post('/api/addTask',task.create);
+    app.post('/api/deleteTask/id=:id',task.delete);
+    app.post('/api/updateTask/:id',task.update);
+    app.get('/api/taskByProgLang/:id',task.findByProgLanguage);
+    app.get('/api/taskByUser/:id',task.findByUserId);
+    app.get('/api/uploadedTasks',task.findByFileUploaded);
+    app.get('/api/taskByDiscipline/:id',task.findByTeacherDisciplineId)
 }
